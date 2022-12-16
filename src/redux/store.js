bc95cac11a98f966ddfc62355cfd2d91aeecce95
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-12-16 18:50:00 +0000

    Install redux 8 to fix data fetching problem

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENTION__ ? window.__REDUX_DEVTOOLS_EXTENTION__() : f => f
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 );
 
